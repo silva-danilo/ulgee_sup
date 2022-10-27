7d90984a3d1ad26b32a81c626f86d624d3437ee2
silva-danilo <111794967+silva-danilo@users.noreply.github.com>
2022-10-27 12:04:13 -0300

    Update cb.R

--- application/cb.R
+++ application/cb.R
@@ -1,4 +1,3 @@
-
 # confidence band
 par(mar=c(5.5,5.5,2,2), mfrow=c(1,3))
 
@@ -13,11 +12,12 @@ eta.i <- eta - sd.z
 prob <- exp(eta)/(1+exp(eta))
 prob.s <- exp(eta.s)/(1+exp(eta.s))
 prob.i <- exp(eta.i)/(1+exp(eta.i))
-plot(z[,2], prob, type="n", ylim=c(0,0.7), xlab="gini", ylab="Fitted psewage", cex.axis=1.2, cex.lab=1.5)
-polygon(c(z[,2], rev(z[,2])), c(prob.i, rev(prob.s)), col="red", border=NA)
+plot(z[,2], prob, type="n", ylim=c(0,0.7), xlab="gini", ylab="Fitted psewage", cex.axis=1.5, cex.lab=1.8)
+polygon(c(z[,2], rev(z[,2])), c(prob.i, rev(prob.s)), col=rainbow(3, s=0.65)[1], border=NA)
 lines(z[,2], prob, lty=2,lwd=2)
 lines(z[,2], prob.s,lwd=2)
 lines(z[,2], prob.i,lwd=2)
+legend("top", col=rainbow(3, s=0.65)[1], legend=c("1991"), cex=1.5, bty="n", title="Year:", horiz=T, lwd=2, inset=c(0,.03))
 #points(data$gini[time==1], y[time==1], pch=16, cex=1.5)
 
 # 2000
@@ -31,11 +31,12 @@ eta.i <- eta - sd.z
 prob <- exp(eta)/(1+exp(eta))
 prob.s <- exp(eta.s)/(1+exp(eta.s))
 prob.i <- exp(eta.i)/(1+exp(eta.i))
-plot(z[,4], prob, type="n", ylim=c(0,0.7), xlab="gini", ylab="Fitted psewage", cex.axis=1.2, cex.lab=1.5)
-polygon(c(z[,4], rev(z[,4])), c(prob.i, rev(prob.s)), col="green", border=NA)
+plot(z[,4], prob, type="n", ylim=c(0,0.7), xlab="gini", ylab="Fitted psewage", cex.axis=1.5, cex.lab=1.8)
+polygon(c(z[,4], rev(z[,4])), c(prob.i, rev(prob.s)), col=rainbow(3, s=0.65)[2], border=NA)
 lines(z[,4], prob, lty=2,lwd=2)
 lines(z[,4], prob.s,lwd=2)
 lines(z[,4], prob.i,lwd=2)
+legend("top", col=rainbow(3, s=0.65)[2], legend=c("2000"), cex=1.5, bty="n", title="Year:", horiz=T, lwd=2, inset=c(0,.03))
 #points(data$gini[time==2], y[time==2], pch=16, cex=1.5)
 
 # 2010
@@ -49,9 +50,10 @@ eta.i <- eta - sd.z
 prob <- exp(eta)/(1+exp(eta))
 prob.s <- exp(eta.s)/(1+exp(eta.s))
 prob.i <- exp(eta.i)/(1+exp(eta.i))
-plot(z[,7], prob, type="n", ylim=c(0,0.7), xlab="gini", ylab="Fitted psewage", cex.axis=1.2, cex.lab=1.5)
-polygon(c(z[,7], rev(z[,7])), c(prob.i, rev(prob.s)), col="blue", border=NA)
+plot(z[,7], prob, type="n", ylim=c(0,0.7), xlab="gini", ylab="Fitted psewage", cex.axis=1.5, cex.lab=1.8)
+polygon(c(z[,7], rev(z[,7])), c(prob.i, rev(prob.s)), col=rainbow(3, s=0.65)[3], border=NA)
 lines(z[,7], prob, lty=2,lwd=2)
 lines(z[,7], prob.s,lwd=2)
 lines(z[,7], prob.i,lwd=2)
+legend("top", col=rainbow(3, s=0.65)[3], legend=c("2010"), cex=1.5, bty="n", title="Year:", horiz=T, lwd=2, inset=c(0,.03))
 #points(data$gini[time==3], y[time==3], pch=16, cex=1.5)
