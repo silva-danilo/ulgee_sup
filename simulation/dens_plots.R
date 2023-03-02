9861f596d092d622dd039d8fbcfbb41826238940
Danilo Silva <danilo.silva@ime.usp.br>
2023-03-02 09:49:59 -0300

    Add files via upload

--- simulation/dens_plots.R
+++ simulation/dens_plots.R
@@ -1,18 +1,22 @@
-# load ulgee package
-library(ulgee)
+
+# source ulgee
+#source("R/ulgee.R")
 
 # density function
 par(mar=c(5.5,5.5,2,2), mfrow=c(1,2))
 mu <- c(0.2, 0.4, 0.6, 0.8)
 dul_y <- function(y) dul(y, mu=0.2)
-curve(dul_y, lty=1, ylab="Density", xlab="y", cex.lab=1.5, cex.axis=1.2, lwd=2, ylim=c(0,6.5))
+curve(dul_y, lty=1, ylab="Density", xlab=expression(italic(y)), cex.lab=1.5, 
+      cex.axis=1.2, lwd=2, ylim=c(0,6.5))
 for(i in 2:length(mu)){
   dul_y <- function(y) dul(y, mu=mu[i])
   curve(dul_y, add=TRUE, lty=i, lwd=2)
 }
-#text <- c(); for(i in 2:length(mu)) text <- c(text, bquote(~ mu == .(mu[i])))
-legend("top", lty=1:length(mu), legend=mu, cex=0.8, bty="n", title=expression(paste("Values of ", mu, ":")), horiz=T, inset=c(0,.03))
+legend("top", lty=1:length(mu), legend=mu, cex=0.8, bty="n", 
+       title=expression(paste("Values of ", italic("\u03bc"), ":")), 
+       horiz=T, inset=c(0,.03))
 
 # variance
-curve(ul.var, lty=1, ylab="Variance", xlab=expression(mu), cex.lab=1.5, cex.axis=1.2, lwd=2, from=0.01, to=0.99)
+curve(ul.var, lty=1, ylab="Variance", xlab=expression(italic("\u03bc")), 
+      cex.lab=1.5, cex.axis=1.2, lwd=2, from=0.01, to=0.99)
 abline(v=0.5, lty=2, lwd=2)
--- simulation/simulation.R
+++ simulation/simulation.R
@@ -1,5 +1,6 @@
-# load ulgee package
-library(ulgee)
+
+# source ulgee
+#source("R/ulgee.R")
 
 # prep. simulation
 beta <- c(-3, 6)
@@ -11,9 +12,11 @@ s <- c(10, 5, 3)
 # AR1
 set.seed(4598)
 table_1 <- ul_sim(beta, R, rho, n, s, "AR1", "EXC")
-print(xtable::xtable(table_1[,-2], digits=4), type="latex", include.rownames=F, include.colnames=F)
+print(xtable::xtable(table_1[,-2], digits=4), type="latex", 
+      include.rownames=F, include.colnames=F)
 
 # EXC
 set.seed(8878)
 table_2 <- ul_sim(beta, R, rho, n, s, "EXC", "AR1")
-print(xtable::xtable(table_2[,-2], digits=4), type="latex", include.rownames=F, include.colnames=F)
+print(xtable::xtable(table_2[,-2], digits=4), type="latex", 
+      include.rownames=F, include.colnames=F)
